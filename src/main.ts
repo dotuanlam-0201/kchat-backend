69f538fa61407177fba90ebe7e9d1cb6d079e737
dotuanlam-0201 <dotuanlam212k@gmail.com>
2025-05-08 11:58:50 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://kchat-app.netlify.app/'],
-  });
+  app.enableCors();
   app.useGlobalInterceptors(new TransformInterceptor());
   const config = new DocumentBuilder()
     .setTitle('Telegram Service')
